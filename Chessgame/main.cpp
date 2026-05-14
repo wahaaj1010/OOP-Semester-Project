1012e9b9dafe4b7419333d03a3b335127b5a757d
Wahaaj Humayun <f250100@cfd.nu.edu.pk>
2026-05-14 19:55:52 +0500

    Update main.cpp
    Added Help Menu and Clear Screen function

--- Chessgame/main.cpp
+++ Chessgame/main.cpp
@@ -2,8 +2,37 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+// Clear the terminal screen
+void clearScreen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+// Show help instructions
+void showHelp() {
+    clearScreen();
+    cout << "\n  +----------------------------------+\n";
+    cout << "  |          HOW TO PLAY             |\n";
+    cout << "  +----------------------------------+\n\n";
+    cout << "  Enter moves in this format:  e2 e4\n\n";
+    cout << "  File (column) : a - h  (left to right)\n";
+    cout << "  Rank (row)    : 1 - 8  (bottom to top)\n\n";
+    cout << "  Pieces:  P = Pawn    R = Rook\n";
+    cout << "           N = Knight  B = Bishop\n";
+    cout << "           Q = Queen   K = King\n\n";
+    cout << "  White pieces = UPPERCASE\n";
+    cout << "  Black pieces = lowercase\n\n";
+    cout << "  Press Enter to go back...\n";
+    cin.get();
+}
+
 int main() {
     while (true) {
+        clearScreen();
         cout << "\n  +----------------------------------+\n";
         cout << "  |          CHESS GAME              |\n";
         cout << "  +----------------------------------+\n\n";
@@ -21,6 +50,7 @@ int main() {
         } else if (choice == "2") {
             showHelp();
         } else if (choice == "3") {
+            clearScreen();
             cout << "\n  Goodbye!\n\n";
             break;
         } else {
