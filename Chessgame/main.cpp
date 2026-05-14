7d86370da2f6a662ecd9d14cf9de2c11ea21e860
Wahaaj Humayun <f250100@cfd.nu.edu.pk>
2026-05-14 19:30:58 +0500

    Update main.cpp
    Added  a Menu Screen with 3 options

--- Chessgame/main.cpp
+++ Chessgame/main.cpp
@@ -1,7 +1,32 @@
 #include "Game.h"
-
+#include <iostream>
+#include <string>
+using namespace std;
 int main() {
-    Game game;
-    game.run();
+    while (true) {
+        cout << "\n  +----------------------------------+\n";
+        cout << "  |          CHESS GAME              |\n";
+        cout << "  +----------------------------------+\n\n";
+        cout << "  1.  Start Game\n";
+        cout << "  2.  Help\n";
+        cout << "  3.  Exit\n\n";
+        cout << "  Choose an option: ";
+
+        string choice;
+        getline(cin, choice);
+
+        if (choice == "1") {
+            Game game;
+            game.run();
+        } else if (choice == "2") {
+            showHelp();
+        } else if (choice == "3") {
+            cout << "\n  Goodbye!\n\n";
+            break;
+        } else {
+            cout << "\n  Invalid option. Press Enter to try again...\n";
+            cin.get();
+        }
+    }
     return 0;
 }
